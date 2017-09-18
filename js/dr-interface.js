a6dba058e2634632a41cd76a36b70a50c166cc21
nr <nicholas.raethke@gmail.com>
2017-09-18 02:45:21 -0700

    push more logic to UI

--- js/dr-interface.js
+++ js/dr-interface.js
@@ -1,11 +1,15 @@
-var apiKey = require('./../.env').apiKey;
 var drSearchModule = require('./../js/dr.js').drSearchModule;
+var apiKey = require('./../.env').apiKey;
 
 function dataResult(response) {
   // let data = response.data;
 
   response.data.forEach(function(doctor){
-
+    let first_name;
+    let last_name;
+    let visit_address;
+    let website;
+    let accepts_new_patients;
 
 
     return $('.result').append(`<li>$.{data.profile.first_name}</li> <li>${data.profile.last_name}</li> <li>${data.visit_address}</li> <li>${data.website}</li> <li>${data.accepts_new_patients}</li>`);
@@ -17,7 +21,7 @@ $(function(){
     event.preventDefault();
     $('.result').show();
     let name = $('#byDr').val();
-    let result = findDoctors(name, dataResult);
+    let search = DrSearchModule.findDoctors(name, dataResult);
 
   });
 });
--- js/dr.js
+++ js/dr.js
@@ -1,8 +1,8 @@
 var apikey = require('./../.env').apiKey;
 
-let DrSearchModule = function(){
-};
-  DrSearchModule.prototype.findDoctors = function(name, dataResult){
+let DrSearchModule = {
+
+  findDoctors: function(name, dataResult){
     $.ajax({
       url: `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5202,-12.6742,100&skip=2&limit=10&user_key+${apiKey}`,
       type: 'GET',
@@ -20,6 +20,7 @@ let DrSearchModule = function(){
         $('.result').html("There was an error processing your request. Please try again.");
       }
     });
-  };
+  }
+};
 
 exports.drSearchModule = DrSearchModule;
