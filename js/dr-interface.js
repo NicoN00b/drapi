c585c7f6aabdc5682d9721937694698d86b87f3f
nr <nicholas.raethke@gmail.com>
2017-09-18 02:20:08 -0700

    start migration of logic to interface.

--- js/dr-interface.js
+++ js/dr-interface.js
@@ -1,10 +1,22 @@
 var apiKey = require('./../.env').apiKey;
-import { drSearch } from './../js/dr.js';
+var drSearchModule = require('./../js/dr.js').drSearchModule;
 
-$(document).ready(function() {
+function dataResult(response) {
+  // let data = response.data;
+
+  response.data.forEach(function(doctor){
+    return $('.result').append(`<li>$.{data.profile.first_name}</li> <li>${data.profile.last_name}</li> <li>${data.visit_address}</li> <li>${data.website}</li> <li>${data.accepts_new_patients}</li>`);
+  });
+}
+
+$(function(){
   $('#searchDr').submit(function(event) {
     event.preventDefault();
-    $('.result').html("");
-    findDoctors();
-  })
+    $('.result').show();
+    let name = $('#byDr').val();
+    let result = findDoctors(name, dataResult);
+
+  });
 });
+
+// $('.result').html(`<li>$.{data.profile.first_name}</li> <li>${data.profile.last_name}</li> <li>${data.visit_address}</li> <li>${data.website}</li> <li>${data.accepts_new_patients}</li>`);
--- js/dr.js
+++ js/dr.js
@@ -1,20 +1,23 @@
 var apikey = require('./../.env').apiKey;
 
-export let drSearch = {
-  findDoctors: function request(){
+let DrSearchModule = function(){
+};
+  DrSearchModule.prototype.findDoctors = function(name, dataResult){
     $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5202,-12.6742,100&skip=2&limit=10&user_key= + apiKey`,
+      url: `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5202,-12.6742,100&skip=2&limit=10&user_key+${apiKey}`,
       type: 'GET',
       data: {
         format: 'json'
       },
       success: (response) => {
-        let data = $.parseJSON(response);
-        $('#result').html(`<li>${data.profile.first_name}</li> <li>${data.profile.last_name}</li> <li>${data.visit_address}</li> <li>${data.website}</li> <li>${data.accepts_new_patients}</li>`);
+        dataResult(response);
+
+
       },
       error: function() {
-        $('#errors').html("There was an error processing your request. Please try again.")
+        $('#errors').html("There was an error processing your request. Please try again.");
       }
     });
-  }
-};
+  };
+
+exports.drSearchModule = DrSearchModule;
